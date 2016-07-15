d6adb570b7e39166b709cc1c77bd8a4e8adbaf6a
Chris Paris <cap@chrisparis.org>
2016-07-15 10:00:47 -1000

    Just cream and water

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,5 +5,17 @@ module Lib
 import Data.LinearProgram
 import Data.LinearProgram.GLPK
 
+objFun :: LinFunc String Double
+objFun = varSum ["cream", "water"]
+
+lp :: LP String Double
+lp = execLPM $ do
+  setDirection Min
+  setObjective objFun
+  constrain' "Fat" (linCombination [(0.36, "cream"), (0.0, "water")]) (Equ 0.18)
+  constrain' "Total" (varSum ["cream", "water"]) (Equ 1.0)
+  varBds "cream" 0 1
+  varBds "water" 0 1
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print =<< glpSolveVars simplexDefaults lp
