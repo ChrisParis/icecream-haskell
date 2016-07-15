3f5614ed765d4a4bd9d23e3a38c7d3793e34b2d3
Chris Paris <cap@chrisparis.org>
2016-07-15 10:52:22 -1000

    Build constraints from Ingredients and Properties

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Lib
     ( someFunc
     ) where
@@ -10,6 +12,7 @@ data Ingredient = Ingredient
   , fat :: Double
   , sweetness :: Double
   , solids :: Double
+  , bounds :: Bounds Double
   }
   deriving Eq
 
@@ -26,6 +29,7 @@ cream = Ingredient
   , fat = 0.36
   , sweetness = 0
   , solids = 0.45
+  , bounds = Bound 0 1
   }
 
 water = Ingredient
@@ -33,6 +37,7 @@ water = Ingredient
   , fat = 0
   , sweetness = 0
   , solids = 0
+  , bounds = Bound 0 1
   }
 
 ingredients = [cream, water]
@@ -42,6 +47,13 @@ properties = [ Property "Fat" fat (Equ 0.18)
              , Property "Solids" solids (Bound 0.2 1.0)
              ]
 
+buildConstraints properties ingredients = do
+  let buildConstraint (Property label get bounds) =
+        constrain' label (linCombination (zip (fmap get ingredients) ingredients)) bounds
+      buildVarBounds ingredient = setVarBounds ingredient $ bounds ingredient
+  mapM_ buildConstraint properties
+  mapM_ buildVarBounds ingredients
+
 objFun :: LinFunc Ingredient Double
 objFun = varSum ingredients
 
@@ -49,11 +61,8 @@ lp :: LP Ingredient Double
 lp = execLPM $ do
   setDirection Min
   setObjective objFun
-  constrain' "Fat" (linCombination [(0.36, cream), (0.0, water)]) (Equ 0.18)
-  constrain' "Solids" (linCombination [(0.45, cream), (0.0, water)]) (Bound 0.2 1.0)
+  buildConstraints properties ingredients
   constrain' "Total" (varSum ingredients) (Equ 1.0)
-  varBds cream 0 1
-  varBds water 0 1
 
 someFunc :: IO ()
 someFunc = print =<< glpSolveVars simplexDefaults lp
